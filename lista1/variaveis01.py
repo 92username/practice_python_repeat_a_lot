a7138a0aabf49d71183919353dc24f57a80b3507
92username <115041664+92username@users.noreply.github.com>
2024-12-22 20:43:20 -0300

    lynt: docstrings and too long lines

--- lista1/variaveis01.py
+++ lista1/variaveis01.py
@@ -1,7 +1,12 @@
+"""
+Lista de exercicios - Manipulação de variáveis
+"""
 ## 1. Manipulação de Variáveis
 
 ### Exercício 1:
-# Crie uma variável chamada `nome` e atribua o valor do seu nome a ela. Em seguida, crie outra variável chamada `idade` e atribua sua idade. Exiba no console:
+# Crie uma variável chamada `nome` e atribua o valor do seu nome a 
+# ela. Em seguida, crie outra variável chamada `idade` e atribua sua idade. 
+# Exiba no console:
 # ```
 # Meu nome é <nome> e tenho <idade> anos.
 # ```
@@ -12,7 +17,8 @@ idade = 45
 print(f"O meu nome é {nome} e tenho {idade}")
 
 ### Exercício 2:
-# Atribua valores a três variáveis `a`, `b`, e `c`. Troque os valores de `a` e `b` 
+# Atribua valores a três variáveis `a`, `b`, e `c`. 
+# Troque os valores de `a` e `b` 
 # sem usar uma variável auxiliar e exiba o resultado.
 
 a = 5
@@ -23,7 +29,8 @@ a,b = b,a
 print(f"A nova lista após a troca é {a}, {b} e {c}")
 
 # ### Exercício 3:
-# Calcule a área de um círculo. Peça ao usuário o valor do raio e armazene o resultado em uma variável chamada `area`. A fórmula para a área é:
+# Calcule a área de um círculo. Peça ao usuário o valor do raio e 
+# armazene o resultado em uma variável chamada `area`. A fórmula para a área é:
 # ```
 # area = pi * raio^2
 # ```
@@ -35,7 +42,8 @@ area = pi * (raio ** 2)
 print(f"A área do círculo é {area:.4f}")
 
 ### Exercício 4:
-# Atribua uma string de texto contendo um número (por exemplo, "25") a uma variável. 
+# Atribua uma string de texto contendo um número (por exemplo, "25") a 
+# uma variável. 
 # Converta essa string para um número inteiro e exiba o dobro do valor.
 
 algumtexto = "25"
@@ -43,20 +51,24 @@ algumtexto = int(algumtexto)
 print(f"O dobro do valor é {algumtexto * 2}")
 
 ### Exercício 5:
-# Crie duas variáveis: `preco` e `desconto`. Atribua valores a elas. Calcule o preço final após o desconto e exiba o resultado.
+# Crie duas variáveis: `preco` e `desconto`. Atribua valores a elas. 
+# Calcule o preço final após o desconto e exiba o resultado.
 
 preco = 3000
 desconto = 0.25
 preco_final = preco - (preco * desconto)
-print(f"O preço original é {preco:.2f} reais. O valor do desconto é 25%. O preço com desconto é {preco_final:.2f} reais.")
+print(f"O preço original é {preco:.2f} reais. O valor do desconto é 25%. 
+      O preço com desconto é {preco_final:.2f} reais.")
 
-# Crie uma variável chamada nome e outra chamada idade. Atribua valores a elas e exiba:
+# Crie uma variável chamada nome e outra chamada idade. Atribua valores 
+# a elas e exiba:
 # Meu nome é <nome> e tenho <idade> anos.
 nome = "Kardian"
 idade = 1
 print(f"Meu carro é um {nome} e ele tem {idade} ano de uso.")
 
-# Crie duas variáveis a e b com valores inteiros. Troque os valores sem usar uma variável auxiliar.
+# Crie duas variáveis a e b com valores inteiros. Troque os valores sem 
+# usar uma variável auxiliar.
 
 a ,b = 7, 49 # multiple assignment or unpacking
 print(f"As variáveis originais são {a} e {b}")
@@ -72,7 +84,8 @@ subtracao = x - y
 multiplicacao = x * y
 divisao = x / y
 
-print(f"A soma é {soma}, a subtração é {subtracao}, a multiplicação é {multiplicacao} e a divisão é {divisao:.2f}")
+print(f"A soma é {soma}, a subtração é {subtracao}, a multiplicação é 
+      {multiplicacao} e a divisão é {divisao:.2f}")
 
 # defina 3 variaveis raio, pi e circunferencia. calcule a circunferencia.
 
